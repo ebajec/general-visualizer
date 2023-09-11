396f4e1a107b1cd4ad8707dc885e7122fbc2f6d5
elijah <20ebd1@queensu.ca>
2023-09-11 08:19:54 -0400

    now uses cmake

---
+++ build/CMakeFiles/visualizer.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visualizer.dir/src/camera.cpp.o"
+  "CMakeFiles/visualizer.dir/src/camera.cpp.o.d"
+  "CMakeFiles/visualizer.dir/src/main.cpp.o"
+  "CMakeFiles/visualizer.dir/src/main.cpp.o.d"
+  "CMakeFiles/visualizer.dir/src/shader.cpp.o"
+  "CMakeFiles/visualizer.dir/src/shader.cpp.o.d"
+  "CMakeFiles/visualizer.dir/src/shapes.cpp.o"
+  "CMakeFiles/visualizer.dir/src/shapes.cpp.o.d"
+  "CMakeFiles/visualizer.dir/src/vertex.cpp.o"
+  "CMakeFiles/visualizer.dir/src/vertex.cpp.o.d"
+  "CMakeFiles/visualizer.dir/src/view_window.cpp.o"
+  "CMakeFiles/visualizer.dir/src/view_window.cpp.o.d"
+  "visualizer"
+  "visualizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
